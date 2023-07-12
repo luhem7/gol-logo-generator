b201ffebbebc274174f276a6f8d00116e05b0767
luhem7 <mehul.gang@gmail.com>
2023-07-11 21:41:38 -0400

    Creating logos of multiple sizes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-mod geom_helpers;
-
-use crate::geom_helpers::{calc_eucledian_distance, Point, Viewport};
+use std::fs;
 use image::{imageops::FilterType, DynamicImage};
 
+mod geom_helpers;
+use crate::geom_helpers::{calc_eucledian_distance, Point, Viewport};
+
+
+struct ImgSize (u32, u32);
 
 fn main() {
-    let final_size = (100, 100);
-    let imgx: u32 = 800;
-    let imgy: u32 = 800;
+    let logo_sizes: [ImgSize; 2] = [
+        ImgSize(100, 100),
+        ImgSize(200, 200),
+    ];
+    let source_size = ImgSize(800, 800);
+    let imgx: u32 = source_size.0;
+    let imgy: u32 = source_size.1;
 
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
@@ -23,16 +30,23 @@ fn main() {
         let distance_to_center = calc_eucledian_distance(&circle_center, &curr_point);
 
         if distance_to_center <= circle_radius {
-            *pixel = image::Rgb([255, 255, 255]);
-        } else {
             let r = (0.6 * x as f32) as u8;
             let b = (0.6 * y as f32) as u8;
             *pixel = image::Rgb([r, 0, b]);
+        } else {
+            *pixel = image::Rgb([255, 255, 255]);
         }
     }
 
-    let scaled = DynamicImage::from(imgbuf).resize(100, 100, FilterType::Lanczos3);
-    // imgbuf = image::imageops::blur(&imgbuf, 1.0);
+    let directory_path = "outputs";
+    // Check if the directory exists
+    if fs::metadata(directory_path).is_err() {
+        // Directory does not exist, so create it
+        fs::create_dir(directory_path).expect("Failed to create directory");
+    }
 
-    scaled.save("./outputs/logo.png").unwrap();
+    for logo_size in logo_sizes {
+        let scaled = DynamicImage::from(imgbuf.clone()).resize(logo_size.0, logo_size.1, FilterType::Lanczos3);
+        scaled.save(format!("./{}/logo_{}x{}.png", directory_path, logo_size.0, logo_size.1)).unwrap();
+    }
 }
